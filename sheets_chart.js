16ec43476288e81bee2a0c01c98bb2399e7a4180
Gaston Jouglard <gastonjouglard.10@gmail.com>
2025-01-09 06:32:01 +1000

    Separete Methods

--- sheets_chart.js
+++ sheets_chart.js
@@ -2,8 +2,25 @@ document.addEventListener('DOMContentLoaded', () => { async () => {
     const projectName = document.getElementById('projectName').textContent;
     // Datos simulados: reemplaza esto con la llamada al backend para obtener el JSON
     const apiUrl = `https://script.google.com/macros/s/AKfycby0imLlKjegWFr29LKgHWEa4RdaApP7Au8h2i3jdcrvH6GuBbyVmuhKjP898Bq4tvuf/exec`;
-    const sheetsData = await fetch(`${apiUrl}?action=getSheets&project=${encodeURIComponent(projectName)}`)
-        .then(response => response.json());
+    
+    try {
+        const response = await fetch(`${apiUrl}?action=getSheets&project=${encodeURIComponent(projectName)}`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok '+ response.statusText);
+        }
+        const sheetsData = await response.json();
+
+        generateChart(sheetsData);
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
+}});
+
+function generateChart(sheetsData) {
+    if (sheetsData.length === 0) {
+        console.error('No data to display');
+        return;
+    }
 
     // 1. Preparar datos para el gráfico
     const sheets = sheetsData.map(sheet => sheet.Sheet);
@@ -61,4 +78,6 @@ document.addEventListener('DOMContentLoaded', () => { async () => {
             }
         }
     });
-}});
+}
+
+    
