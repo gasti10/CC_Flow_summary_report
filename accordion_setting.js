6e3b90961c6047a336ddd628da4e397ba64da6be
Gaston Jouglard <gastonjouglard.10@gmail.com>
2025-01-10 09:23:39 +1000

    Add accordion_setting.js

---
+++ accordion_setting.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    new Accordion('#accordion', {
+        duration: 300,
+        showMultiple: false,
+        collapse: true,
+    });
+
+    // Selecciona todos los encabezados del acordeón
+    const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+    accordionHeaders.forEach(header => {
+        header.addEventListener('click', () => {
+            // Obtener el ítem padre
+            const accordionItem = header.parentElement;
+
+            // Alternar la clase "active"
+            const isActive = accordionItem.classList.contains('active');
+            document.querySelectorAll('.accordion-item.active').forEach(item => {
+                item.classList.remove('active');
+            });
+
+            if (!isActive) {
+                accordionItem.classList.add('active');
+            }
+        });
+    });
+});
