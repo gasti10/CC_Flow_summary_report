5e3a77fbcbf1d33bdbe612f57014976afcd8fe81
Gaston Jouglard <gastonjouglard.10@gmail.com>
2025-01-10 09:01:22 +1000

    Accordion style

--- sheets_chart.js
+++ sheets_chart.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    console.log('DOM completamente cargado y procesado');
     const projectName = document.getElementById('projectName').textContent;
     if (!projectName) throw new Error("Project name not found");
     // Datos simulados: reemplaza esto con la llamada al backend para obtener el JSON
