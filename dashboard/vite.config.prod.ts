6de0a6a044286e02e7f8546754326e682404d762
Gaston Jouglard <gastonjouglard.10@gmail.com>
2025-07-07 22:36:05 -0300

    Update

--- dashboard/vite.config.prod.ts
+++ dashboard/vite.config.prod.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // Configuración específica para producción
 export default defineConfig({
   plugins: [react()],
-  base: '/cc-flow-dashboard/',
+  base: '/CC_Flow_summary_report/dashboard/',
   build: {
     outDir: 'dist',
     target: 'es2015',
